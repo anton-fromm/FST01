2dfb9534df3aaa7b7b8b01d398c5c128a1cff7a3
Stadtler, Sebastian <s.stadtler@douglas.de>
2019-06-17 16:01:44 +0200

    Add Locationbased Logic

--- Sourcecode/FST.TournamentPlanner/FST.TournamentPlanner.UI/ViewModel/Messages/GenerateWinnerCertificatesMessage.cs
+++ Sourcecode/FST.TournamentPlanner/FST.TournamentPlanner.UI/ViewModel/Messages/GenerateWinnerCertificatesMessage.cs
@@ -8,14 +8,21 @@ namespace FST.TournamentPlanner.UI.ViewModel.Messages
 {
     public class GenerateWinnerCertificatesMessage : GalaSoft.MvvmLight.Messaging.MessageBase
     {
-        public GenerateWinnerCertificatesMessage(string firstPlace, string secondPlace)
+        public GenerateWinnerCertificatesMessage(string firstPlace, string secondPlace, string tournamentName, string tournamentPlace, DateTime tournamentEndDate)
         {
             FirstPlace = firstPlace;
             SecondPlace = secondPlace;
+            TournamentName = tournamentName;
+            TournamentPlace = tournamentPlace;
+            TournamentEndDate = tournamentEndDate;
         }
 
         public string FirstPlace { get; private set; }
 
         public string SecondPlace { get; private set; }
+        public string TournamentName { get; private set; }
+        public string TournamentPlace { get; private set; }
+        public DateTime TournamentEndDate { get; private set; }
+
     }
 }
