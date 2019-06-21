fea609f936107c49ce9739a283706a7bdfed1107
René Stakemeier <rstakemeier@web.de>
2019-06-21 22:10:20 +0200

    RC1

--- Sourcecode/FST.TournamentPlanner/FST.TournamentPlanner.UI/View/Converter/MatchResultTeamNameDecorationConverter.cs
+++ Sourcecode/FST.TournamentPlanner/FST.TournamentPlanner.UI/View/Converter/MatchResultTeamNameDecorationConverter.cs
@@ -10,7 +10,7 @@ using System.Windows.Data;
 
 namespace FST.TournamentPlanner.UI.View
 {
-    public class MatchResultTeamNameDecorationConverter : IValueConverter
+    public class MatchResultTeamNameDecorationConverter : IValueConverter, IMultiValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -55,5 +55,50 @@ namespace FST.TournamentPlanner.UI.View
         {
             throw new NotImplementedException();
         }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var team = parameter.ToString();
+            var obj = values[0] as MatchViewModel;
+            if (obj == null)
+            {
+                return Binding.DoNothing;
+            }
+            if (obj.State != 2)
+            {
+                return Binding.DoNothing;
+            }
+            if (obj.Winner.Equals(obj.TeamOne))
+            {
+                // Team One is the winner
+                if (team == "One")
+                {
+                    return Binding.DoNothing;
+                }
+                if (team == "Two")
+                {
+                    return TextDecorations.Strikethrough;
+                }
+            }
+            if (obj.Winner.Equals(obj.TeamTwo))
+            {
+                // Team One is the winner
+                if (team == "One")
+                {
+                    return TextDecorations.Strikethrough;
+                }
+                if (team == "Two")
+                {
+                    return Binding.DoNothing;
+                }
+            }
+            return Binding.DoNothing;
+        }
+
     }
 }
